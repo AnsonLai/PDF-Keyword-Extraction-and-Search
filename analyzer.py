aa7e98c466960030a4d6eeaa5f191abde9c0bde4
Anson Lai <ansonhwlai@gmail.com>
2020-11-15 13:41:24 -0800

    cleanup

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,5 @@
 import fitz
+# SECRETS lists IBM Watson Natural Language api and url, and a link to a sample text file
 from SECRETS import apikey, url, service_url
 
 import json
@@ -6,50 +7,50 @@ from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, KeywordsOptions
 
-# # PREPARE PLAIN TEXT FILES
-# doc = fitz.open('sample.pdf')
-# plain_text = ''
+# PREPARE PLAIN TEXT FILES
+doc = fitz.open('sample.pdf')
+plain_text = ''
 
-# for page in doc:
-#   page_plain_text = page.getTextPage().extractText().replace("-\n", "").replace("\n", "")
-#   plain_text += page_plain_text
+for page in doc:
+  page_plain_text = page.getTextPage().extractText().replace("-\n", "").replace("\n", "")
+  plain_text += page_plain_text
 
-# text_file = open("sample.txt", "w", encoding="utf-8")
-# n = text_file.write(plain_text)
-# text_file.close()
+text_file = open("sample.txt", "w", encoding="utf-8")
+n = text_file.write(plain_text)
+text_file.close()
 
-# # SPLIT INTO 50,000 CHUNKS
-# i = 0
-# while plain_text != '':
-#   split_plain_text = ''
-#   if len(plain_text) > 50000:
-#     split_plain_text = plain_text[0:50000]
-#     plain_text = plain_text[50000:]
-#   else:
-#     split_plain_text = plain_text[0:]
-#     plain_text = ''
-#   text_file = open("sample_split" + str(i) + ".txt", "w", encoding="utf-8")
-#   n = text_file.write(split_plain_text)
-#   text_file.close()
-#   i += 1
+# SPLIT INTO 50,000 CHUNKS
+i = 0
+while plain_text != '':
+  split_plain_text = ''
+  if len(plain_text) > 50000:
+    split_plain_text = plain_text[0:50000]
+    plain_text = plain_text[50000:]
+  else:
+    split_plain_text = plain_text[0:]
+    plain_text = ''
+  text_file = open("sample_split" + str(i) + ".txt", "w", encoding="utf-8")
+  n = text_file.write(split_plain_text)
+  text_file.close()
+  i += 1
 
-# # KEYWORD EXTRACTION
-# authenticator = IAMAuthenticator(apikey)
-# natural_language_understanding = NaturalLanguageUnderstandingV1(
-#     version='2020-08-01',
-#     authenticator=authenticator
-# )
-# natural_language_understanding.set_service_url(service_url)
+# KEYWORD EXTRACTION
+authenticator = IAMAuthenticator(apikey)
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2020-08-01',
+    authenticator=authenticator
+)
+natural_language_understanding.set_service_url(service_url)
 
-# for j in range(i):
+for j in range(i):
 
-#   text = open("sample_split" + str(j) + ".txt", "r", encoding="utf-8").read()
-#   response = natural_language_understanding.analyze(text = text, features=Features(keywords=KeywordsOptions(limit=4500))).get_result()
+  text = open("sample_split" + str(j) + ".txt", "r", encoding="utf-8").read()
+  response = natural_language_understanding.analyze(text = text, features=Features(keywords=KeywordsOptions(limit=4500))).get_result()
 
-#   ibm_output = json.dumps(response, indent=2)
-#   json_file = open("ibm_output_" + str(j) + ".json", "w", encoding="utf-8")
-#   n = json_file.write(ibm_output)
-#   json_file.close()
+  ibm_output = json.dumps(response, indent=2)
+  json_file = open("ibm_output_" + str(j) + ".json", "w", encoding="utf-8")
+  n = json_file.write(ibm_output)
+  json_file.close()
 
 # PARSE JSON FILES FOR KEYWORDS
 keywords = []
@@ -70,12 +71,11 @@ keywords = sorted(list(set(keywords)), key=str.lower)
 
 # SEARCH
 keyword_locations ={}
-
 doc = fitz.open('sample.pdf')
 
-for page in doc.pages:
+for page in doc:
   page_rect = page.MediaBox
-  # TODO: May mannually offset to match printed page numbers]
+  # TODO: May mannually offset to match printed page numbers
   page_num = page.number
 
   for keyword in keywords:
@@ -99,6 +99,10 @@ for page in doc.pages:
 sample_output = json.dumps(keyword_locations, indent=2)
 json_file = open("sample_output.json", "w", encoding="utf-8")
 n = json_file.write(sample_output)
+
+
+
+
 
 
 
